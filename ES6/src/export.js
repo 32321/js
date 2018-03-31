37ef9e40a360d05cce2c1ec42557854f49d7386e
zhulihao965255 <2623165118@qq.com>
2018-03-31 11:53:33 +0800

    ES6  模块

---
+++ ES6/src/export.js
@@ -0,0 +1,13 @@
+// export let a=10;
+//
+// export class People{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+//     self(){
+//         console.log(`${this.name}的年龄是:${this.age}`);
+//     }
+// }
+let c;
+export default c = (x,y) => x*y;
---
+++ ES6/src/import.js
@@ -0,0 +1,7 @@
+// import {a,People,l} from "./export.js";
+// console.log(a);
+//
+// let p1=new People("zs",18);
+// p1.self();
+import l from "./export.js";
+console.log(l(3,5));
